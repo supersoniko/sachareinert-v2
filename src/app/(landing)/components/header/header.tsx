ce7a1abb9db4a52d58e73ae930fdf43107188c2f
Sacha <sacha.reinert@outlook.com>
2024-03-17 10:08:59 +0100

    Desktop header

--- src/app/(landing)/components/header/header.tsx
+++ src/app/(landing)/components/header/header.tsx
@@ -1,26 +1,38 @@
 import Link from "next/link";
 import { LetterEffectText } from "../letter-effect-text/letter-effect-text";
 
+const links = [
+  {
+    label: "Pet Project",
+    href: "#pet-project",
+  },
+  {
+    label: "Contributions",
+    href: "#oss-contributons",
+  },
+  {
+    label: "Contact",
+    href: "#contact",
+  },
+];
+
 export const Header = () => {
   return (
-    <div className="flex justify-between border-b bg-card px-12 py-4">
+    <div className="flex items-center justify-between border-b bg-card pl-12">
       <div>
         <span className="text-4xl font-semibold">Sacha </span>
         <span className="text-4xl">Reinert</span>
       </div>
       <nav className="flex space-x-4">
-        <Link href="#contact">
-          <LetterEffectText text="Contact" />
-        </Link>
-        <Link href="#contact">
-          <LetterEffectText text="Contact" />
-        </Link>
-        <Link href="#contact">
-          <LetterEffectText text="Contact" />
-        </Link>
-        <Link href="#contact">
-          <LetterEffectText text="Contact" />
-        </Link>
+        {links.map((link) => (
+          <Link
+            className="flex h-16 w-36 items-center justify-center border-l-2"
+            key={link.href}
+            href={link.href}
+          >
+            <LetterEffectText text={link.label} />
+          </Link>
+        ))}
       </nav>
     </div>
   );
