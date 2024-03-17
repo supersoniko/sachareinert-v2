1783488f1eaffc0113b8f7768755f947332d85f7
Sacha <sacha.reinert@outlook.com>
2024-03-17 15:41:46 +0100

    Mobile design

--- src/app/(landing)/components/footer/footer.tsx
+++ src/app/(landing)/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer className="flex items-center space-x-8 border-b bg-card px-12 py-6">
+    <footer className="flex items-center space-x-8 border-b bg-card px-4 py-6 md:px-12">
       <Link
         className="flex space-x-1"
         target="_blank"
--- src/app/(landing)/components/section-layout/section-layout.tsx
+++ src/app/(landing)/components/section-layout/section-layout.tsx
@@ -3,5 +3,10 @@ import { ComponentPropsWithoutRef } from "react";
 type SectionLayoutProps = ComponentPropsWithoutRef<"section">;
 
 export const SectionLayout = ({ ...props }: SectionLayoutProps) => {
-  return <section {...props} className="flex pl-12"></section>;
+  return (
+    <section
+      {...props}
+      className="flex flex-col pl-4 md:flex-row md:pl-12"
+    ></section>
+  );
 };
