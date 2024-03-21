2e5b8bdef8b8958d5421293e7b653ddbda34d507
Sacha <sacha.reinert@outlook.com>
2024-03-21 23:51:33 +0100

    Wrap star canvas in suspense

--- src/app/(landing)/components/cube-cloud/cube-cloud-canvas.tsx
+++ src/app/(landing)/components/cube-cloud/cube-cloud-canvas.tsx
@@ -7,6 +7,7 @@ import { CubeCloud } from "./cube-cloud";
 import { cn } from "@/lib/utils";
 
 import styles from "./cube-cloud-canvas-wrapper.module.css";
+import { Suspense } from "react";
 
 export function CubeCloudCanvasWrapper({
   children,
@@ -19,32 +20,34 @@ export function CubeCloudCanvasWrapper({
         className={cn("-z-40", styles.gridChild)}
         gl={{ antialias: false, alpha: false }}
       >
-        <color args={[0x111111]} attach="background" />
-        <ambientLight intensity={Math.PI} />
-        <Stars
-          radius={100}
-          depth={50}
-          count={5000}
-          factor={4}
-          saturation={0}
-          fade
-          speed={1}
-        />
-
-        <CubeCloud n={20} spread={20} />
-
-        <EffectComposer
-          enableNormalPass={false}
-          frameBufferType={HalfFloatType}
-          multisampling={0}
-        >
-          <Bloom
-            luminanceThreshold={0.95}
-            luminanceSmoothing={1.0}
-            intensity={4}
-            mipmapBlur
+        <Suspense fallback={null}>
+          <color args={[0x111111]} attach="background" />
+          <ambientLight intensity={Math.PI} />
+          <Stars
+            radius={100}
+            depth={50}
+            count={5000}
+            factor={4}
+            saturation={0}
+            fade
+            speed={1}
           />
-        </EffectComposer>
+
+          <CubeCloud n={20} spread={20} />
+
+          <EffectComposer
+            enableNormalPass={false}
+            frameBufferType={HalfFloatType}
+            multisampling={0}
+          >
+            <Bloom
+              luminanceThreshold={0.95}
+              luminanceSmoothing={1.0}
+              intensity={4}
+              mipmapBlur
+            />
+          </EffectComposer>
+        </Suspense>
       </Canvas>
       <div className={styles.gridChild}>{children}</div>
     </div>
