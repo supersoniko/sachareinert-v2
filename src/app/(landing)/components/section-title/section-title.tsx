e15ffba454ef06fbd89a6f22b5979ed5981e92d9
Sacha <sacha.reinert@outlook.com>
2024-03-21 21:16:37 +0100

    Lightning text for section titles

--- src/app/(landing)/components/section-title/section-title.tsx
+++ src/app/(landing)/components/section-title/section-title.tsx
@@ -5,7 +5,7 @@ type SectionTitleProps = ComponentPropsWithoutRef<"h2">;
 
 export const SectionTitle = (props: SectionTitleProps) => {
   return (
-    <div className="flex space-x-2">
+    <div className="lightning-text flex space-x-2">
       <CornerDownRight size={32} />
       <h2 {...props} className="mb-4 text-3xl md:mb-0 md:w-[20vw]" />
     </div>
