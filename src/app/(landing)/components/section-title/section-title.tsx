54d6f87f705f219670daa7e743dba2216e1bcf6c
Sacha <sacha.reinert@outlook.com>
2024-03-22 16:59:45 +0100

    Resolve build issue

--- src/app/(landing)/components/section-title/section-title.tsx
+++ src/app/(landing)/components/section-title/section-title.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { CornerDownRight } from "lucide-react";
 import { ComponentPropsWithoutRef } from "react";
 
@@ -5,7 +6,7 @@ type SectionTitleProps = ComponentPropsWithoutRef<"h2">;
 
 export const SectionTitle = (props: SectionTitleProps) => {
   return (
-    <div className="drop-shadow-lightning flex space-x-2">
+    <div className={cn("flex space-x-2", "drop-shadow-lightning")}>
       <CornerDownRight size={32} />
       <h2 {...props} className="mb-4 text-3xl md:mb-0 md:w-[20vw]" />
     </div>
